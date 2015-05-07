19236b90ba42d58b7bfec3827c131aad449293c0
Malwine <malwine@mailbox.org>
2015-05-07 14:12:34 +0200

    Fill sieve with multiples

--- src/ss2015_aufgabe3/Eratosthenes.java
+++ src/ss2015_aufgabe3/Eratosthenes.java
@@ -2,18 +2,42 @@ package ss2015_aufgabe3;
 
 import java.util.Set;
 import java.util.TreeSet;
-
+/**
+ * Eratosthenes creates an object with two sets 
+ * to find the prime numbers in the given range of numbers.
+ * @author Malwine
+ *
+ */
 public class Eratosthenes {
 	Set<Integer> all;
 	Set<Integer> sieve;
 	
+	/**
+	 * Constructor of an Eratosthenes object, 
+	 * which created the object and two sorted sets.
+	 * @param n
+	 */
 	public Eratosthenes(int n) {
-		
-		all = new TreeSet<Integer>();
-		
-		for(int i = 0; i < n; i++){
-			all.add((Integer)i);
-			System.out.println(i);
+		all = new TreeSet<Integer>();	//create sorted set "all"
+		for(int i = 2; i <= n; i++){  
+			all.add((Integer)i);     	//fill set with number from 2 to n
 		}
+
+		sieve = new TreeSet<Integer>(); 	  //create sorted set "sieve"
+		for(int i = 2; i <= n; i++){     	  //for all numbers n
+			for(int p = 2; p <= n; p++){ 
+				int multiple = i * p;    	  //find all multiples of all n * n(p) numbers
+				sieve.add((Integer)multiple); //fill set with multiples
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public TreeSet<Integer> findPrimes(){
+		//TODO
+		return null;
 	}
 }
--- src/ss2015_aufgabe3/EratosthenesTest.java
+++ src/ss2015_aufgabe3/EratosthenesTest.java
@@ -1,16 +1,36 @@
 package ss2015_aufgabe3;
 
+//import java.util.Iterator;
+
 public class EratosthenesTest {
 
 	public static void main(String[] args) {
 		
 		Eratosthenes e1 = new Eratosthenes(5);
 		
-		System.out.println(e1.all.isEmpty());
-		System.out.println(e1.all.size());
-		System.out.println(e1.all.toString());
-		System.out.println(e1.all.contains((Integer)1));
-		System.out.println(e1.all.contains((Integer)5));
+		System.out.println("isEmpty: " + e1.all.isEmpty());
+		System.out.println("Size: " + e1.all.size());
+		System.out.println("toString: " + e1.all.toString());
+		System.out.println("contains(1): " + e1.all.contains((Integer)1));
+		System.out.println("contains(5): " + e1.all.contains((Integer)5));
+		
+		/*
+		e1.all.remove((Integer)2);
+		e1.all.remove((Integer)3);
+		System.out.println("toString: " + e1.all.toString());
+		e1.all.add((Integer)2);
+		System.out.println("toString: " + e1.all.toString());
+		*/
+		
+		System.out.println();
+		System.out.println("toString: " + e1.sieve.toString());
+		
+		/*//Go through set.
+		Iterator<Integer> it = e1.sieve.iterator();
+		while(it.hasNext()){
+			System.out.println((Integer)it.next());
+		}
+		*/
 	}
 
 }
